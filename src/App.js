7420c5dafa848a3ce4e9e6fff04c906d2c4265dc
村本充輝 <[mitsuki1235epsilon@yahoo.co.jp]>
2021-08-31 08:07:34 +0900

    learn props-type of component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import PropTypes from'prop-types';
+
 function App() {
   const profiles = [
     {name:"Mitsuki",age:22},
     {name:"Ryusei",age:20},
-    {name:"NoName"}
+    {name:"Kazuhisa",age:50}
   ]
   return (
     <div>
@@ -23,8 +25,9 @@ const User = (props) =>{
   </div>)
 }
 
-User.defaultProps = {
-  age : 1
+User.propTypes = {
+  name : PropTypes.string,
+  age : PropTypes.number.isRequired
 }
 
 export default App;
